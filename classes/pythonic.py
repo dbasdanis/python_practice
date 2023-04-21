304e56c25d37409428c8c9ad2bd4c1fe7373b662
dbasdanis <dbasdanis97@gmail.com>
2023-04-21 09:56:51 +0200

    feat: add more comments

--- classes/pythonic.py
+++ classes/pythonic.py
@@ -1,8 +1,13 @@
 import math
 
 class Ring(object):
+    """ Here we will see the actual logic behind various pieces of Python
+    language e.g. instances, variables, method and @property etc.
+    Also, we will see the combine usage of these pieces to complete a
+    design with Agile methodology.
+    """
 
-    def _init_(self, date, metal, radius, price, quantity):
+    def __init__(self, date, metal, radius, price, quantity):
         """ init is not the constructor, but the initializer which
         initialize the instance variable
 
@@ -14,7 +19,7 @@ class Ring(object):
         self.radius, self.metal etc. are the instance variable which
         must be unique.
         """
-        
+
         self.date = date
         self.metal = metal
         self.radius = radius
@@ -23,6 +28,6 @@ class Ring(object):
 
     def cost(self):
         return self.price * self.quantity
-    
+
     def area(self):
         return math.pi * self.radius**2
