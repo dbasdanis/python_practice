eef9b6a8ad9e994ba18856f480a107421c0cf5bb
dbasdanis <dbasdanis97@gmail.com>
2023-04-21 10:33:53 +0200

    feat: class variables

--- classes/pythonic.py
+++ classes/pythonic.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 class Ring(object):
     """ Here we will see the actual logic behind various pieces of Python
@@ -7,7 +8,12 @@ class Ring(object):
     design with Agile methodology.
     """
 
-    def __init__(self, date, metal, radius, price, quantity):
+    # class variables
+    date = time.strftime("%Y-%m-%d", time.gmtime()) # today's date "YYYY-mm-dd"
+    center = 0.0 # center of the ring
+
+    def __init__(self, date=date, metal="Copper", radius=5.0,
+                price=5.0, quantity=5):
         """ init is not the constructor, but the initializer which
         initialize the instance variable
 
