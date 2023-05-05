b4d8cdb0260dc82c334bc4a6688dd4bb78a725ef
dbasdanis <dbasdanis97@gmail.com>
2023-05-05 14:43:28 +0200

    feat: add methods

--- classes/geometry.py
+++ classes/geometry.py
@@ -3,3 +3,12 @@ class PPoint:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0]<self.x<upright[0] and lowleft[1]<self.y<upright[1]:
+            return True
+        else:
+            return False
+        
+    def distance_from_point(self, point):
+        return ((self.x - point.x)**2+(self.y - point.y)**2) ** 0.5
