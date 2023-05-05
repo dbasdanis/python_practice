9de874300c5fa79b43e6be34585a9df85a8d1a67
dbasdanis <dbasdanis97@gmail.com>
2023-05-05 15:43:25 +0200

    feat: add geometry_game.py

---
+++ classes/geometry_game.py
@@ -0,0 +1,13 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+
+turtle.done()
