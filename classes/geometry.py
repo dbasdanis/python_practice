b25dd77ed32eec47c54408af8ba514af0d916ff1
dbasdanis <dbasdanis97@gmail.com>
2023-05-05 15:27:35 +0200

    feat: upgrade geometry

--- classes/geometry.py
+++ classes/geometry.py
@@ -4,8 +4,8 @@ class PPoint:
         self.x = x
         self.y = y
 
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0]<self.x<upright[0] and lowleft[1]<self.y<upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x<self.x<rectangle.upright.x and rectangle.lowleft.y<self.y<rectangle.upright.y:
             return True
         else:
             return False
