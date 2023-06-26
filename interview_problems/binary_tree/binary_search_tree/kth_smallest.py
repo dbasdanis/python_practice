96598a0e6793e52ba261cc895b0186a596d22a4b
dbasdanis <dbasdanis97@gmail.com>
2023-06-26 12:30:25 +0200

    feat: add TreeNode class on a different folder

--- interview_problems/binary_tree/binary_search_tree/kth_smallest.py
+++ interview_problems/binary_tree/binary_search_tree/kth_smallest.py
@@ -1,9 +1,5 @@
 from typing import Optional
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from treeNode import TreeNode
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         stack = []
