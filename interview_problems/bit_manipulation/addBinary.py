c83388e6ec5d5471448830243e55a41abc6cb1be
dbasdanis <dbasdanis97@gmail.com>
2023-06-27 16:32:33 +0200

    feat: add binary add

---
+++ interview_problems/bit_manipulation/addBinary.py
@@ -0,0 +1,33 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        carry = 0
+        
+        i, j = len(a)-1, len(b)-1
+        
+    
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            
+            result.append(str(total % 2))
+            
+            carry = total // 2
+            
+        return ''.join(reversed(result))
+            
+        
+        
+            
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.addBinary("111","11")
