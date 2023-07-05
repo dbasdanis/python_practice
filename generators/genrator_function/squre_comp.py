1892eacd8d8489dc6352d040b967ab77758c7ef0
dbasdanis <dbasdanis97@gmail.com>
2023-07-05 14:03:40 +0200

    feat: add convert type, loop and next

--- generators/genrator_function/squre_comp.py
+++ generators/genrator_function/squre_comp.py
@@ -6,3 +6,18 @@ print(my_list)
 # my_list = (expression for element in iterable)
 my_list = (num ** 2 for num in range(1,10))
 print(my_list)
+
+# convert type
+# print(list(my_list))
+print(set((num ** 2 for num in range(1,10))))
+print(tuple((num ** 2 for num in range(1,10))))
+
+# loop
+for num in (num ** 2 for num in range(1,10)):
+    print(num)
+
+# next
+print(next(my_list))
+print(next(my_list))
+print(next(my_list))
+print(next(my_list))
