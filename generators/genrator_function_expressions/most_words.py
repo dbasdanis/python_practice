efe555c9e3f8f584e79537276e83ab3501c12d43
dbasdanis <dbasdanis97@gmail.com>
2023-07-05 18:20:18 +0200

    fix: rename the folder

---
+++ generators/genrator_function_expressions/most_words.py
@@ -0,0 +1,13 @@
+words = [
+    "able lost darkness learn",
+    "road older goes",
+    "basic",
+    "must seed laid itself hot must",
+    "compound decide",
+    "trap active paragraph hair review stay written",
+    "facing smile vowel chose other",
+    "occur shop box metal equal mouse some city"
+]
+
+most = max((len(string.split(" ")) for string in words))
+print(most)
