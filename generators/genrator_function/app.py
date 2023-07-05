7817f2935c1940544a468daf2cbd097da5cfa5e4
dbasdanis <dbasdanis97@gmail.com>
2023-07-05 13:39:55 +0200

    feat: use next and loop

--- generators/genrator_function/app.py
+++ generators/genrator_function/app.py
@@ -10,6 +10,17 @@ def contains_i(words):
 gen_ob = contains_i(some_words)
 
 # convert tpye 
+# it can be used once, then we need to call the function again
 print(list(gen_ob))
 print(set(contains_i(some_words)))
 print(tuple(contains_i(some_words)))
+
+for el in contains_i(some_words):
+    print(el)
+
+# next
+print(next(gen_ob))
+print(next(gen_ob))
+print(next(gen_ob))
+print(next(gen_ob))
+print(next(gen_ob))
